f7ac579b2cec8a93076f23553cd0c2cce362a9d1
m3rashid <m3rashid.hussain@gmail.com>
2024-03-20 01:15:23 +0530

    ok ok complete

--- src/pages/contents.tsx
+++ src/pages/contents.tsx
@@ -13,7 +13,7 @@ function DirectoryContents() {
     const res = await invoke<DriveContent[]>('get_directory_contents', {
       dirPath: dirPath.split('/app')[1],
     });
-    res.sort((a, b) => (a.is_dir ? -1 : 1));
+    res.sort((a, _) => (a.is_dir ? -1 : 1));
     setDriveContents(res);
   }
 
@@ -25,17 +25,20 @@ function DirectoryContents() {
     <>
       <button onClick={() => navigate(-1)}>Go Back</button>
 
-      <div className='m-0 p-4 max-h-[calc(100vh-24px)] min-h-[calc(100vh-24px)] overflow-y-auto flex flex-col bg-[#2f2f2f] gap-4'>
+      <div className='m-0 p-4 max-h-[calc(100vh-24px)] min-h-[calc(100vh-24px)] overflow-y-auto flex flex-wrap bg-[#2f2f2f] gap-4'>
         {driveContents.map((driveFile) => {
           return (
             <div
               key={driveFile.name}
               onClick={() => navigate('/app' + driveFile.mount_point || '')}
-              className={`cursor-pointer rounded-md w-full p-1 pl-6 flex ${
-                driveFile.is_dir ? 'bg-[#7b7b7b69]' : ''
-              }`}
+              className={`cursor-pointer rounded-md p-1 flex flex-col items-center justify-center w-32 'bg-[#7b7b7b69]`}
             >
-              {driveFile.name + '  ' + driveFile.mount_point}
+              <img
+                width={100}
+                height={100}
+                src={driveFile.is_dir ? '/folder.ico' : '/file.ico'}
+              />
+              <p className='break-all text-center'>{driveFile.name}</p>
             </div>
           );
         })}
